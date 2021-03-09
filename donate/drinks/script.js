ea4007a8d47677e8b1490726d2b28cc33413f3bd
kelecn <15766679865@163.com>
2021-03-09 15:09:15 +0800

    Site updated: 2021-03-09 15:09:10

--- donate/drinks/script.js
+++ donate/drinks/script.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function () {
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>jQuery(document).ready(function () {
 
 var thisPage = $('body');
 var drink_box = $('#donate-box');
